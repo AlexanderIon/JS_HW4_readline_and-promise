06bfdcb67ff3019bea22677b1a3d678f9b40bc44
AlexanderIon <tov.sandro@mail.ru>
2022-08-13 15:42:01 +0300

    правка задач 1 и 2

--- taskOne.js
+++ taskOne.js
@@ -1,40 +1,41 @@
-function checkedPassword(a){
-    let password = a
-    return function () {
+function checkedPassword(my_pw = 0,write_down_pw = 0){
+    let password = write_down_pw
+    return function (password) {
     
     
-        if (a === "1" ){
+        if ( password === my_pw ){
             return true
         }
         else {
+            
             return false
         }
     }
 }
 
+// const checked = checkedPassword("3r2")
+// console.log(checked("r2"))
+
+
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 const { runInContext } = require('node:vm');
-
 const rl = readline.createInterface({ input, output });
-
-
-
-
-// const w = checkedPassword("qqqq")
+const w = checkedPassword("q")
 // console.log(w())
 
 rl.setPrompt("WRITE DOWN password: ")
 rl.prompt()
 rl.on('line', answer=>{
-    let res = checkedPassword(answer)
     
-    if ( res() === true){ 
+    
+    if ( w(answer) === true){ 
         console.log("УДАЧНО")       
         rl.close()
-        // console.log(res)
+        //console.log(res)
     }
     else {
+        console.log()
         rl.prompt()
     }
 } )
--- taskThree.js
+++ taskThree.js
@@ -2,7 +2,7 @@ function getIntNumber(max){
     return Math.floor(Math.random()*max)
 }
 
-function complit(a,b){
+function compare(a,b){
     let res = {result:false,msg:""}
     
     if (a<b){
@@ -34,33 +34,99 @@ function count(start = 1){
 }
 
 
+
+
+console.log("the start of the  PROGRAM")
+
+const PromiseCompare = (some_a,some_b) => {
+    const promise = new Promise((resolve,rejct)=>{
+        setTimeout(()=>{
+            let res = compare(some_a,some_b);
+            resolve (res)
+        },2000)
+    })
+    return promise
+}
+
+const dbSelect = (query) => {
+    const promise = new Promise((resolve,rejct)=>{
+        setTimeout(()=>{
+            console.log(`ДЕЛАЕМ ЗАПРОС ${query} в БД`)
+            resolve ('ДАННЫЕ ИЗ БД ')
+        },3000)
+    })
+    return promise
+}
+
+// console.log(PromiseCompare)
+// console.log(PromiseCompare(20,40))
+// const prcompare1 = PromiseCompare(60,40)
+// const prcompare2 = PromiseCompare(20,50)
+
+// PromiseCompare(40,40)
+//     .then((value) =>{
+//         console.log(value.msg)
+//     })
+
+//     prcompare1.then((value)=>{
+//         console.log(value.msg)
+//     })
+//     prcompare2.then((value)=>{
+//         console.log(value.msg)
+//     })
+//     .then((value)=>{
+//          // ТУТ ИДЕТ ВЫЗОВ dbSelect("SELECT") как достать  resolve dbSelect??????
+//          return dbSelect("SELECT") 
+//     })    
+
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 const { runInContext } = require('node:vm');
+const { resolve } = require('node:path')
+const { rejects } = require('node:assert')
 const guessNumber = getIntNumber(30)
 const rl = readline.createInterface({ input, output });
-const my_count =count()
-rl.setPrompt("УКАЖИ ЧИСЛО: ")
-rl.prompt()
 
-function ask(number) {
-    return new Promise(resolve => {
-      rl.on('line', answer => {
-        if (complit(answer,number).result === false){
-            console.log(`${complit(answer,number).msg} это потыка № ${my_count()} `)
+function  MyPlay(some_number) {
+    
+ 
+    const my_count =count()
+    rl.setPrompt("УКАЖИ ЧИСЛО: ")
+    rl.prompt()
+    rl.on("line",your_number=>{
+        
+        if (compare(your_number,some_number).result === false){
+            console.log(`${compare(your_number,some_number).msg} это потыка № ${my_count()} `)
             rl.prompt()
-            resolve = true
-            
         }
         else{
             console.log(`ТЫ угадал с ${my_count()} попытки`)
             rl.close()
-            resolve = false
         }
+            
+    })
+}
+
+const PromiseMyPlay = (e)=>{
+    const promise = new Promise((resolve,reject)=>{
+        resolve (MyPlay(e))
+    })
+    return promise
+}
+
+// MyPlay(30)
+PromiseMyPlay(40)
+    .then(()=>{
         
-      });
-    });
-  }
-  
-  ask(guessNumber)
-    .then(result => { console.log(result); return ask(); })
+    })
+    // .then(()=>{
+        // ТУТ ИДЕТ ВЫЗОВ dbSelect("SELECT") как достать  resolve dbSelect??????
+    //     return dbSelect ("SELECT")
+    // })
+console.log("the END of the  PROGRAM")
+
+
+
+
+
+
